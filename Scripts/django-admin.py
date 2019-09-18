743cedf90bd463218a6e4a8e4c1c8b28882014ed
pmarcelojr <marcelosantostecnologia@gmail.com>
2019-09-18 13:03:26 -0300

    Commit Inicial

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\marcelo.santos\desktop\projeto-web\python\workspace\api_rest\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
